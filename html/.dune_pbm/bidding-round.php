c85b4942819750335e25429b3fe8036884b4af6e
Paul Untredies <dune.pbm@gmail.com>
2017-11-18 19:35:44 -0800

    Worked on bidding round.

--- html/.dune_pbm/bidding-round.php
+++ html/.dune_pbm/bidding-round.php
@@ -14,22 +14,46 @@ if (empty($_POST)){
     //##############################################################
     if (!isset($game['biddingRound'])) {
         $game['biddingRound'] = array();
+        $game['biddingRound']['numberOfCards'] = 6; //I know this is wrong;
+        $game['biddingRound']['biddingOrder'] = $game['meta']['playerOrder'];
+        $game['biddingRound']['highBid'] = 0;
+        $game['biddingRound']['highBidder'] = '';
     }
     
     //##############################################################
     //## Every Run #################################################
     //##############################################################
-	echo 
-	'<h2>Bidding Round</h2>';
+	echo '<h2>Bidding Round</h2>';
+	
+	// Shows card to Atredies
+	if ($_SESSION('faction') == '[A]') {
+		echo '<p>Card up for bid: '.$game['treachery']['deck'][0].'.';
+	}
+	
+	echo '<p>Current high bid: '.$game['biddingRound']['highBid'].
+		' by '.$game['biddingRound']['highBidder'].'.</p>';
+	
+	echo
+    '<form action="#" method="post">
+    <p>Bid 
+        <input id="bid" name="bid" type="number" min='.
+			($game['biddingRound']['highBid']+1).'max=100 value="0"/>
+			<input type="submit" value="Submit">
+    </p></form>';
+    
+    echo
+    '<form action="" method="post">
+    <button name="closeBidding" value="closeBidding">Close Bidding</button>
+    </form>';
 }
 
 //######################################################################
 //###### Post ##########################################################
 //######################################################################
-if (isset($_POST['stormAction'])) {
-    if ($_POST['storm_action'] == 'done') {
-        dune_readData();
-        $game['meta']['next'][$_SESSION['faction']] = 'wait.php';
+if (isset($_POST['bid'])) {
+    if ($_POST['bid'] > $game['biddingRound']['highBid']) {
+        $game['biddingRound']['highBid'] = $_POST['bid'];
+        $game['biddingRound']['highBider'] = $_SESSION['faction'];
     }
         
     //##############################################################
